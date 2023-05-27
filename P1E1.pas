f44275cf73c49a9d88575fa4b0ff07f0519df561
Gael Cano <124222953+GaelCanoDev@users.noreply.github.com>
2023-05-27 00:53:41 -0300

    Add files via upload

---
+++ P1E1.pas
@@ -0,0 +1,14 @@
+program Entrenamiento;
+{Implemente un programa que lea por teclado dos números enteros e imprima en pantalla los
+valores leídos en orden inverso. Por ejemplo, si se ingresan los números 4 y 8, debe mostrar el
+mensaje: Se ingresaron los valores 8 y 4}
+
+var
+	n1, n2: integer;
+begin
+	writeln ( 'Ingrese un numero: ' );
+	readln ( n1 );
+	writeln ( 'Ingrese otro numero' );
+	readln (n2);
+	writeln ( 'Los numeros leidos de manera inversa fueron: ', n2 ,' y ' ,n1 );
+end.
